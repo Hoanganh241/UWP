a91ee81f0d871c94978d0dbfcf89b540e27b878e
Nguyễn Hoàng Anh <73330650+Hoanganh241@users.noreply.github.com>
2021-11-21 12:07:16 +0700

    i

---
+++ Fried_Chicken/Fried_Chicken/Adapters/SQLiteHelper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLitePCL;
+using Windows.Storage;
+using System.IO;
+
+namespace Fried_Chicken.Adapters
+{
+    class SQLiteHelper
+    {
+        private readonly string dbame = "chicken.db";
+
+        private static SQLiteHelper instance;
+
+
+        private SQLiteHelper()
+        {
+            string path = Path.Combine(ApplicationData.Current.LocalFolder.Path, dbName);
+            SQLiteConnection = new SQLiteConnection(path); //tao ket noi db SQLite
+
+
+            //Tao bang Cart: Id Name Image, Price, Qty
+            var sql_txt = @"create table if not exists Cart(Id integer primary key, Name var(255), Image varchar(255), Price integer, Qty integer)";
+            var statement = SQLiteConnection.Prepare(sql_txt);
+            statement.Step();
+        }
+        public SQLiteConnection SQLiteConnection { get; set; }
+
+
+        
+        public static SQLiteHelper GetInstance()
+        {
+            if (instance == null)
+                instance = new SQLiteHelper();
+            return instance;
+
+        }
+
+    }
+}
---
+++ Fried_Chicken/Fried_Chicken/Services/CartService.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fried_Chicken.Models.Entity;
+using SQLitePCL;
+using Fried_Chicken.Adapters;
+namespace Fried_Chicken.Services
+{
+    interface ICartService
+    {
+        List<CartItem> GetCart();// abtract method
+        bool AddToCart(CartItem item);
+        bool RemoveItem(CartItem item);
+        bool UpdateItem(CartItem item, int newQty);
+        bool ClearCart();
+    }
+    class CartService : ICartService
+    {
+        public bool AddToCart(CartItem item)
+        {
+            try
+            {
+                ISQLiteConnection connection = SQLiteHelper.GetInstance().SQLiteConnection;
+                var sql_txt = "insert into Cart(Id, Name, Image, Price, Qty) values(?,?,?,?,?)";
+                var statement = connection.Prepare(sql_txt);
+                statement.Bind(1, item.Id);
+                statement.Bind(2, item.Name);
+                statement.Bind(3, item.Image);
+                statement.Bind(4, item.Price);
+                statement.Bind(5, item.Qty);
+                var rs = statement.Step();
+                return rs == SQLiteResult.OK;
+                
+            }
+            catch(Exception e)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public bool ClearCart()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<CartItem> GetCart()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveItem(CartItem item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateItem(CartItem item, int newQty)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
